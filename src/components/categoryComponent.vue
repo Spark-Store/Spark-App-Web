fb2ecf6d3dafab4516f22e4faaf5eb1a0578b1af
陌生人 <moshengrenx@foxmail.com>
2020-07-15 20:23:46 +0800

    抽出应用卡片

--- src/components/categoryComponent.vue
+++ src/components/categoryComponent.vue
@@ -1,65 +1,53 @@
 <template>
   <div class="app">
-    <br/>
-    <br/>
+    <br />
+    <br />
     <center>
       <div class="list">
-        <div class="item" v-for="item in list" :key="item.tip">
-          <span class="show">
-            <img :src="`http://dcstore.shenmo.tech/store/${category}/${item.Pkgname}/icon.png`" alt="icon"
-                 class="icon-m" @click="GotoJson(item.Pkgname)"/>
-            <span class="app-title">
-             <h3 class="app-name">{{item.Name}}</h3>
-           </span>
-            <a target="_blank">
-              <button class="download-bt" :title="downloadContent" @click="GotoJson(item.Pkgname)">
-                详情>>
-              </button>
-            </a>
-          </span>
-        </div>
+        <appCard
+          class="item"
+          v-for="item in list"
+          :key="item.tip"
+          :downloadContent="downloadContent"
+          :Pkgname="item.Pkgname"
+          :Name="item.Name"
+          :category="category"
+        ></appCard>
       </div>
     </center>
   </div>
 </template>
 
 <script>
-  import axios from "axios";
-  import Vue from "vue";
-
-  export default {
-    name: "categoryComponent",
-    props: ['category'],
-    data() {
-      return {
-        list: [],
-        downloadContent: "DOWNLOAD"
-      };
-    },
-    methods: {
-      getInfo() {
-        axios
-          .get(`http://dcstore.shenmo.tech/store/${this.category}/applist.json`)
-          .then(res => {
-            this.list = res.data;
-          });
-      },
-      GotoJson(pkgn) {
-        console.log(
-          `http://dcstore.shenmo.tech/store/${this.category}/${pkgn}/app.json`
-        );
-        window.open(
-          `http://dcstore.shenmo.tech/store/${this.category}/${pkgn}/app.json`, '_self', ''
-        );
-      }
-    },
-    mounted() {
-      this.getInfo();
+import axios from "axios";
+import Vue from "vue";
+import appCard from "./appCard";
+export default {
+  name: "categoryComponent",
+  props: ["category"],
+  components: { appCard },
+  data() {
+    return {
+      list: [],
+      downloadContent: "DOWNLOAD"
+    };
+  },
+  methods: {
+    getInfo() {
+      axios
+        .get(`http://dcstore.shenmo.tech/store/${this.category}/applist.json`)
+        .then(res => {
+          this.list = res.data;
+        });
     }
-  };
+  },
+  mounted() {
+    this.getInfo();
+  }
+};
 </script>
 
 
 <style scoped>
-  @import "../../static/style.css";
+@import "../../static/style.css";
 </style>
