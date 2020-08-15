64a59851102e53883be7faf1a5b557ff99ec8dee
Yzzi <40852301+uiYzzi@users.noreply.github.com>
2020-08-15 22:20:16 +0800

    更新

--- src/components/categoryComponent.vue
+++ src/components/categoryComponent.vue
@@ -11,7 +11,7 @@
 
           <span class="show">
 <!--            应用icon-->
-            <img :src="`http://img.shenmo.tech:38324/store/${category}/${item.Pkgname}/icon.png`" alt="icon"
+            <img :src="`http://dcstore.shenmo.tech/store/${category}/${item.Pkgname}/icon.png`" alt="icon"
                  class="icon-m" @click="GotoJson(item.Pkgname)"/>
             <span class="app-title">
 <!--              应用名-->
@@ -24,7 +24,7 @@
            </span>
           </span>
 <!--
-          <img :src="`http://img.shenmo.tech:38324/store/${category}/${item.Pkgname}/icon.png`" alt="icon"
+          <img :src="`http://dcstore.shenmo.tech/store/${category}/${item.Pkgname}/icon.png`" alt="icon"
                class="icon-bg" @click="GotoJson(item.Pkgname)"/>
 -->
         </div>
@@ -51,7 +51,7 @@
       getInfo() {
         axios
           //39.106.2.2:38324
-          .get(`http://img.shenmo.tech:38324/store/${this.category}/applist.json`)
+          .get(`http://dcstore.shenmo.tech/store/${this.category}/applist.json`)
           //applist.json 软件列表
           .then(res => {
             this.list = res.data;
@@ -59,12 +59,12 @@
       },
       GotoJson(pkgn) {
         console.log(
-          `http://img.shenmo.tech:38324/store/${this.category}/${pkgn}/app.json`
+          `http://dcstore.shenmo.tech/store/${this.category}/${pkgn}/app.json`
           //app.json 软件的详细信息
         );
         window.open(
           //pkgn 在仓库中的包名
-          `http://img.shenmo.tech:38324/store/${this.category}/${pkgn}/app.json`, '_self', ''
+          `http://dcstore.shenmo.tech/store/${this.category}/${pkgn}/app.json`, '_self', ''
         );
       }
     },
